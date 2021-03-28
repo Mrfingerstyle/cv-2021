15070ca360c82e8e9a4ac62a9873c2bcb1973eac
Mrfingerstyle <FE_liuyoung@163.com>
2021-03-28 13:52:35 +0800

    init cv-2021

---
+++ src/main.js
@@ -0,0 +1,39 @@
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('skills'));
+
+// 指定图表的配置项和数据
+var option = {
+    tooltip: {},
+    radar: {
+        // shape: 'circle',
+        name: {
+            textStyle: {
+                color: '#fff',
+                backgroundColor: '#999',
+                borderRadius: 3,
+                padding: [3, 5]
+            }
+        },
+        indicator: [
+            { name: '静态页面 HTML/CSS/JS', max: 100},
+            { name: '编程基础', max: 100},
+            { name: 'Vue全家桶', max: 100},
+            { name: 'React全家桶', max: 100},
+            { name: '项目开发能力', max: 100},
+            { name: '沟通能力', max: 100}
+        ]
+    },
+    series: [{
+        type: 'radar',
+        // areaStyle: {normal: {}},
+        data: [
+            {
+                value: [100, 70, 90, 80, 85, 70],
+                name: '能力雷达'
+            }
+        ]
+    }]
+};
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
